94b784193600dfb64b5b7b219484ee4f4334d20e
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-06-03 22:27:33 +0530

    Contains Duplicate

--- JAVA/Contains Duplicate.java
+++ JAVA/Contains Duplicate.java
@@ -9,3 +9,15 @@ class Solution {
         return false;
     }
 }
+// seconf approach 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+}
