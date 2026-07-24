5cfd1d1b41f5a1b468dfa05a0510fcf1d49180fb
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-07-24 10:23:04 +0530

    1800. Maximum Ascending Subarray Sum

---
+++ JAVA/1800. Maximum Ascending Subarray Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        
+       int current = nums[0];
+        int best = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+    if (nums[i] > nums[i - 1]) {
+        current += nums[i];
+    } else {
+        current = nums[i];
+    }
+
+    best = Math.max(best, current);
+}
+
+return best;
+    }
+}
