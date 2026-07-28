07d1d8d0ddb8eb109a9ad38ee681ef8b1a4f7b25
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-07-28 10:46:12 +0530

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ JAVA/2441. Largest Positive Integer That Exists With Its Negative.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxK(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int max = -1;
+
+        for (int num : nums) {
+
+            if (set.contains(-num)) {
+
+                max = Math.max(max, Math.abs(num));
+
+            }
+        }
+
+        return max;
+    }
+}
