747eacdd36bcf5e7beebe79cffae68c2629a9d8c
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-08-07 19:12:21 +0530

    367. Valid Perfect Square

---
+++ JAVA/367. Valid Perfect Square.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        while(num == 1){
+            return true;
+        }
+
+        int left = 1;
+        int right = num/2;
+
+        while(left <= right){
+            int mid = (left+right)/2;
+            long sq = (long)mid*mid;
+
+            if(sq == num){
+                return true;
+            }
+            else if(sq > num){
+                right=mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return false;
+    }
+}
