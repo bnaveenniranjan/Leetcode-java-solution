2a67931f646d0fef345861886d2fc54d93f22b39
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-08-10 09:01:40 +0530

    1518. Water Bottles

---
+++ JAVA/1518. Water Bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrunk = numBottles;
+        int empty = numBottles;
+       
+        while( empty >= numExchange){
+            int newFull = empty / numExchange;
+            totalDrunk += newFull;
+            empty = (empty % numExchange) + newFull;
+        }
+        return totalDrunk;
+    }
+}
